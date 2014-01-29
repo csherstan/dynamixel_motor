a44448cd2ef61b95f69b69f4a0e2649ab1856f2a
Craig Sherstan <craig.sherstan@gmail.com>
2014-01-29 11:05:26 -0700

    add JointVelocityController - for velocity based control

---
+++ dynamixel_controllers/src/dynamixel_controllers/joint_velocity_controller.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2010-2011, Antons Rebguns.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of University of Arizona nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import division
+
+
+__author__ = 'Antons Rebguns'
+__copyright__ = 'Copyright (c) 2010-2011 Antons Rebguns'
+__credits__ = 'Cara Slutter'
+
+__license__ = 'BSD'
+__maintainer__ = 'Antons Rebguns'
+__email__ = 'anton@email.arizona.edu'
+
+
+import rospy
+
+from dynamixel_driver.dynamixel_const import *
+from dynamixel_controllers.single_joint_controller import SingleJointController
+
+
+from dynamixel_msgs.msg import JointState
+
+class JointVelocityController(SingleJointController):
+    def __init__(self, dxl_io, controller_namespace, port_namespace):
+        SingleJointController.__init__(self, dxl_io, controller_namespace, port_namespace)
+
+    def process_command(self, msg):
+        speed = msg.data
+        angle = 0
+        if(speed > 0):
+            angle = self.max_angle
+        elif (speed < 0):
+            angle = self.min_angle
+        else:
+            raise NotImplementedError
+
+        self.set_speed(abs(speed))
+
+        mcv = (self.motor_id, self.pos_rad_to_raw(angle))
+        self.dxl_io.set_multi_position([mcv])
+
